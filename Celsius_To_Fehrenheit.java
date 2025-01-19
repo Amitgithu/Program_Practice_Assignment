e18b957aab2760982b77e0b4c7205a349aadee85
Amit Kumar <amit909563@gmail.com>
2025-01-19 15:35:37 +0530

    Modified: Celsius_To_Fehrenheit Program

--- Celsius_To_Fehrenheit.java
+++ Celsius_To_Fehrenheit.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class TemperatureConversion {
+class Conversion {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int celsius = sc.nextInt();
